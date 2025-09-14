48225b26483ab05749226826099341be4f44bf44
eudesbn <bonfigliolieudes@gmail.com>
2025-09-14 13:57:29 -0300

    projeto

--- README.md
+++
@@ -1,1 +0,0 @@
-# QA-Brazil_Python_Automation
---
+++ data.py
@@ -0,0 +1,8 @@
+URBAN_ROUTES_URL = 'https://cnt-f18a4477-67ba-43b2-be73-f8474e01ff64.containerhub.tripleten-services.com?lng=pt'
+
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Pare no bar de sucos'
